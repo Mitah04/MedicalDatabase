e490bb7fc0298822e3bed82350e4429d6e285ba2
Mitah123 <hatim.draoui@ulb.be>
2023-05-15 12:29:05 +0200

    Connection to DB and Queries

--- MedicalDatabase/src/main/java/module-info.java
+++ MedicalDatabase/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module be.ulb.infoh303.project.medicaldatabase {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens be.ulb.infoh303.project.medicaldatabase to javafx.fxml;
     exports be.ulb.infoh303.project.medicaldatabase;
 }
